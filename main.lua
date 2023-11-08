30e8984e77dfaefa362962d946c5d924a39faed2
ldorante <53130160+ldorante@users.noreply.github.com>
2023-11-08 18:47:12 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,15 +2,29 @@
 
 --love.load() is the function that is run one time when the game is run
 function love.load()
-    spaceshipImage = love.graphics.newImage('placeholder.png')
+    spaceshipImage = {}
+    spaceshipImage.x = 400
+    spaceshipImage.y = 500
+    spaceshipImage.speed = 4
+    spaceshipImage.sprite = love.graphics.newImage('placeholder.png')
+    background = love.graphics.newImage('background.png')
 end
 
 --Runs every frame, basically runs everything in here in inf loop as fast as possible
 function love.update(dt)
+    if love.keyboard.isDown("right") then
+        spaceshipImage.x = spaceshipImage.x + spaceshipImage.speed
+    end
+
+    if love.keyboard.isDown("left") then
+        spaceshipImage.x = spaceshipImage.x - spaceshipImage.speed
+    end
 end
 
 --Also runs every frame, this renders graphics on screen
 function love.draw()
     --running this will show you it runs every frame, this randomizes the image's position
-    love.graphics.draw(spaceshipImage, love.math.random(0, 800), love.math.random(0, 600))
+    love.graphics.draw(background, 0, -200)
+    love.graphics.draw(spaceshipImage.sprite, spaceshipImage.x, spaceshipImage.y)
+    
 end
