e2ef0c8e6982c07059f17d805c1e3751870dc1e2
EliMarlow <77943308+EliMarlow@users.noreply.github.com>
2023-11-09 16:03:28 -0500

    added bounds and fixed the image filepath

--- main.lua
+++ main.lua
@@ -6,8 +6,8 @@ function love.load()
     spaceshipImage.x = 400
     spaceshipImage.y = 500
     spaceshipImage.speed = 4
-    spaceshipImage.sprite = love.graphics.newImage('placeholder.png')
-    background = love.graphics.newImage('background.png')
+    spaceshipImage.sprite = love.graphics.newImage('images/placeholder.png')
+    background = love.graphics.newImage('images/background.png')
 end
 
 --Runs every frame, basically runs everything in here in inf loop as fast as possible
@@ -27,6 +27,23 @@ function love.update(dt)
     if love.keyboard.isDown("up") then
         spaceshipImage.y = spaceshipImage.y - spaceshipImage.speed
     end
+
+    if spaceshipImage.x < 0 then
+        spaceshipImage.x = 0
+    end
+
+    if spaceshipImage.x > 750 then
+        spaceshipImage.x = 750
+    end
+
+    if spaceshipImage.y < 0 then
+        spaceshipImage.y = 0
+    end
+
+    if spaceshipImage.y > 700 then
+        spaceshipImage.y = 700
+    end
+    
 end
 
 --Also runs every frame, this renders graphics on screen
